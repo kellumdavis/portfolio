092e110ae5606c4106e1457acba9b7ed80acac58
kellumdavis <kellumdavis@gmail.com>
2022-09-01 18:18:48 -0400

    completed basic styling and layout for projects component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,16 +4,21 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import './Card.css'
+
 
 
 function ProjectCard({image, alt, prjName, prjSum, href }){
 
     const [isShown, setIsShown] = useState(false);
     
+    
+   
+
     return(
         <div>
             <a target="blank" href={href}>
-            <Card sx={{ maxWidth: 300 }}  onMouseEnter={() => setIsShown(true)}
+            <Card sx={{ maxWidth: 300, minWidth: 300, margin: '10px' }}  onMouseEnter={() => setIsShown(true)}
         onMouseLeave={() => setIsShown(false)}>
       <CardActionArea>
         <CardMedia
@@ -21,13 +26,17 @@ function ProjectCard({image, alt, prjName, prjSum, href }){
           height="140"
           image={image}
           alt={alt}
+          style={{objectPosition: 'top'}}
+          
+          
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" color="#0e1c36">
             {prjName}
           </Typography>
           {isShown && (
-          <Typography variant="body2" color="text.secondary">
+        
+          <Typography variant="body2" color="#0e1c36">
             {prjSum}
           </Typography>
           )}
