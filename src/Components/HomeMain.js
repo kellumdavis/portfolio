67ed8bc367781da46462cb583c408821c7a9ecbc
kellumdavis <kellumdavis@gmail.com>
2022-09-14 15:14:12 -0400

    added multiple video formats

--- src/Components/HomeMain.js
+++ src/Components/HomeMain.js
@@ -2,8 +2,8 @@ import React from "react"
 // import {Link} from 'react-router-dom'
 import'./HomeMainStyles.css'
 import heroReelVideo from '../assets/heroReel.mp4'
-
-
+import heroReelMov from '../assets/heroReel.mov'
+import heroReelAvi from '../assets/heroReel.avi'
 function HomeMain(){
 
 
@@ -15,6 +15,8 @@ function HomeMain(){
                 <p>/ Front-end Developer / Software Engineer / </p>
             </div>   
            <video autoPlay loop muted id='video' className="animate__fadeInLeft">
+            <source src={heroReelAvi} type='video/avi'/>
+            <source src={heroReelMov} type='video/mov'/>
             <source src={heroReelVideo} type='video/mp4'/>
            </video>
         </div>
