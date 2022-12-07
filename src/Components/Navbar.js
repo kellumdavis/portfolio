b911c813aa3c828c187e41c0d215fec7e3828b5e
kellumdavis <kellumdavis@gmail.com>
2022-12-07 14:26:03 -0500

    updated projects and resume

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,9 +20,6 @@ function NavBar(){
                     <a href='#about'>About</a>
                 </li>
                 <li>
-                    <a href='#projects'>Projects</a>
-                </li>
-                <li>
                     <a href='#contact'>Contact</a>
                 </li>
             </ul>
